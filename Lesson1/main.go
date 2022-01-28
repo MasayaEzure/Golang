d4f8b6ad6e99526cc75ec365af1b320f49c12e04
MasayaEzure <h7t1s2@gmail.com>
2022-01-28 23:37:05 +0900

    Golangの学習を再開

--- Lesson1/main.go
+++ Lesson1/main.go
@@ -1,6 +1,6 @@
-// パッケージ宣言
+// パッケージの宣言は一つまで
 package main
-// パッケージを取り込む
+
 import (
 	"fmt"
 	"time"
@@ -8,6 +8,61 @@ import (
 
 // メイン関数
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("Hello world")
+	// 現在の時刻を出力
 	fmt.Println(time.Now())
+
+	/*
+	変数
+	明示的な定義
+	var 変数名 型 = 値
+	メイン関数外でも定義できる
+	*/
+	var i int = 100
+	fmt.Println(i)
+
+	var s, t, r string = "aaa", "bbb", "ccc"
+	fmt.Println(s, t, r) 
+
+	// 異なる型で定義
+	var (
+		x int = 123
+		y string = "hello"
+		z bool = true
+	)
+	fmt.Println(x, y, z)
+
+	// 値の更新
+	x = 45
+	y = "Good"
+	z = false
+	fmt.Println(x,y,z)
+
+	/*
+	暗黙的な定義
+	変数名 := 値
+	型指定が不要
+	*/
+	str := "Good Night"
+	number := 678
+	fmt.Println(str, number)
+
+	// 値の更新
+	str = "Good Morning"
+	number = 9
+	fmt.Println(str, number)
+
+	// 変数の出力
+	fmt.Println(aaa)
+
+	// 関数の呼び出し
+	sample()
 }
+
+func sample() {
+	var ex string = "abcdef"
+	fmt.Println(ex)
+
+}
+
+var aaa int = 111
