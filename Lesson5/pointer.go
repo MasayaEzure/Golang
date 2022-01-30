340ab4c43084650f7db729ee26153a846d18eaec
MasayaEzure <h7t1s2@gmail.com>
2022-01-30 15:35:03 +0900

    ポインターの学習を追加

---
+++ Lesson5/pointer.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func Double(i int) {
+	i *= 2
+}
+
+func OtherDouble(i *int) {
+	*i *= 2
+}
+
+func AntherDouble(s []int) {
+	for i, v := range s {
+		s[i] = v * 2
+	}
+}
+
+func main() {
+	var n int = 20
+	fmt.Println(n)
+
+	// メモリ上のアドレスを表示
+	fmt.Println(&n)
+
+	/*
+		ポインタ
+		宣言
+		var ポインタ名 *型 = &変数名
+	*/
+	var p *int = &n
+	// アドレスを出力
+	fmt.Println(p)
+	// 値を出力
+	fmt.Println(*p)
+
+	// 値の更新
+	*p = 30
+	fmt.Println(n)
+
+	n = 50
+	fmt.Println(*p)
+
+	OtherDouble(&n)
+	fmt.Println(n)
+
+	OtherDouble(p)
+	fmt.Println(*p)
+
+	var sl []int = []int{1, 2, 3}
+
+	AntherDouble(sl)
+	fmt.Println(sl)
+}
