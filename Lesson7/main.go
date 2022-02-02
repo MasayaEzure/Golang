26d960a1f69a4248a5c482990a5cf203fbb45a7d
MasayaEzure <h7t1s2@gmail.com>
2022-02-02 23:59:30 +0900

    interfaceの学習を追加

---
+++ Lesson7/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+// インターフェース
+type Stringpy interface {
+	ToString() string
+}
+
+type User struct {
+	Name string
+	Age int
+}
+
+func (u *User) ToString() string {
+	return fmt.Sprintf("Name=%v, Age=%v", u.Name, u.Age) 
+}
+
+type Car struct {
+	Number int
+	Model string
+}
+
+func (c *Car) ToString() string {
+	return fmt.Sprintf("Number=%v, Model=%v", c.Number, c.Model)
+}
+
+// カスタムエラーのinterface
+type ExceptionError struct {
+	// フィールド
+	Message string
+	ErrCode int
+}
+
+func (e *ExceptionError) Error() string {
+	return e.Message
+}
+
+// エラーを返す関数
+func RaiseError() error {
+	return &ExceptionError{
+		Message: "カスタムエラー",
+		ErrCode: 1234,
+	}
+}
+
+func main() {
+	/*
+	i := []Stringpy {
+		&User{Name: "Taro", Age: 25},
+		&Car{Number: 1234, Model: "ABC"},
+	}
+	
+	for _, v := range i {
+		fmt.Println(v.ToString())
+	}
+	*/
+
+	err := RaiseError()
+	fmt.Println(err.Error())
+
+	e, ok := err.(*ExceptionError)
+	if ok {
+		fmt.Println(e.ErrCode)
+	}
+
+}
