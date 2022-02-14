c71245dc54358e52a2f6e6f1a2b3b5b8cbeed6ba
MasayaEzure <h7t1s2@gmail.com>
2022-02-14 23:06:20 +0900

    sortパッケージの学習を追加

--- Lesson10/sort/main.go
+++ Lesson10/sort/main.go
@@ -11,7 +11,27 @@ type Entry struct {
 	Value int
 }
 
+type List []Entry
+
+func (l List) Len() int {
+	return len(l)
+}
+
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+// カスタムする関数
+func (l List) Less(i, j int) bool {
+	if l[i].Value == l[j].Value {
+		return (l[i].Name < l[j].Name)
+	} else {
+		return (l[i].Value < l[j].Value)
+	}
+}
+
 func main() {
+	/*
 	i := []int{5,4,3,2,1,7,8,6,9,0}
 	s := []string{"a", "y", "g"}
 
@@ -42,5 +62,26 @@ func main() {
 	// SliceStable
 	sort.SliceStable(el, func(i, j int) bool {return el[i].Name < el[j].Name})
 	fmt.Println(el)
+	*/
+
+	m := map[string]int{
+		"S": 1,
+		"J": 4,
+		"A": 3,
+		"M": 3,
+	}
+
+	lt := List{}
+	for k, v := range m {
+		e := Entry{k, v}
+		lt = append(lt, e)
+	}
+
+	sort.Sort(lt)
+	fmt.Println(lt)
+
+	// リバース
+	sort.Sort(sort.Reverse(lt))
+	fmt.Println(lt)
 
 }
